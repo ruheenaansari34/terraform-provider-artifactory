5dce8492bcfe8965ceb49e656b26073321386f1e
danielmi <danielmi@jfrog.com>
2022-03-05 18:17:17 -0800

    Test docker run 3

--- scripts/run-artifactory.sh
+++ scripts/run-artifactory.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env sh
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
-echo "${SCRIPT_DIR}"
 export ARTIFACTORY_VERSION=${ARTIFACTORY_VERSION:-7.27.10}
 echo "ARTIFACTORY_VERSION=${ARTIFACTORY_VERSION}"
 
 set -euf
 
-#docker-compose --project-directory ${SCRIPT_DIR} up -d --remove-orphans
-docker run -itd -v /var/run/docker.sock:/var/run/docker.sock -v /root/test/:/var/tmp/ docker/compose:1.24.1  -f docker-compose.yaml up -d
-## test
-docker-compose images
-docker-compose top
-curl -sf -u admin:password http://localhost:8081/artifactory/api/system/licenses/
+docker-compose --project-directory ${SCRIPT_DIR} up -d --remove-orphans
 
 echo "Waiting for Artifactory 1 to start"
-## test
-curl -sf -u admin:password http://localhost:8081/artifactory/api/system/licenses/
 until curl -sf -u admin:password http://localhost:8081/artifactory/api/system/licenses/; do
     printf '.'
     sleep 4
