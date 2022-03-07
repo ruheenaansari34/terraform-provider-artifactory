8f2c04ebf22bc57268e59deeb7186f34ae1bd400
danielmi <danielmi@jfrog.com>
2022-03-07 13:20:57 -0800

    Try docker-compose on the host

--- scripts/run-artifactory-container.sh
+++ scripts/run-artifactory-container.sh
@@ -8,14 +8,8 @@ docker run -i -t -d --rm -v "${SCRIPT_DIR}/artifactory.lic:/artifactory_extra_co
   -p8081:8081 -p8082:8082 -p8080:8080 releases-docker.jfrog.io/jfrog/artifactory-pro:7.24.3
 
 echo "Waiting for Artifactory to start"
-echo "Sleep"
-sleep 60
-docker ps
-curl -sf -u admin:password http://0.0.0.0:8081/artifactory/api/system/licenses/
-
-#echo "Waiting for Artifactory to start"
-#until curl -sf -u admin:password http://localhost:8081/artifactory/api/system/licenses/; do
-#    printf '.'
-#    sleep 4
-#done
-#echo ""
+until curl -sf -u admin:password http://localhost:8081/artifactory/api/system/licenses/; do
+    printf '.'
+    sleep 4
+done
+echo ""
